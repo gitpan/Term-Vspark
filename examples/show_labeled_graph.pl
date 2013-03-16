e18007897590aa50c6b4db2b3a87a6e751b01207
Gil Gonçalves <lursty@gmail.com>
2013-03-16 11:27:52 +0000

    Import of LURST/Term-Vspark-0.30 from CPAN.
    gitpan-cpan-distribution: Term-Vspark
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         LURST/Term-Vspark-0.30.tar.gz
    gitpan-cpan-author:       LURST
    gitpan-cpan-maturity:     released

--- examples/show_labeled_graph.pl
+++ examples/show_labeled_graph.pl
@@ -3,7 +3,7 @@ use warnings;
 
 use Term::Vspark qw[ show_labeled_graph ];
 
-our $VERSION = '0.25'; # VERSION
+# VERSION
 # PODNAME: show_labeled_graph
 
 binmode STDOUT, ':encoding(UTF-8)';
@@ -19,26 +19,3 @@ print show_labeled_graph(
     'columns'  => $columns,
     'k_values' => \%k_values,
 );
-
-__END__
-
-=pod
-
-=encoding utf-8
-
-=head1 NAME
-
-show_labeled_graph
-
-=head1 AUTHOR
-
-Gil Gonçalves <lurst@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2013 by Gil Gonçalves.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
